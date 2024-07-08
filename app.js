7129fe03bdd014355acc55c03d64b53e023040d9
umairghani156 <umairghani156@gmail.com>
2024-07-08 20:31:58 +0500

    added

--- app.js
+++ app.js
@@ -832,3 +832,18 @@ function isDuplicate(arr){
     return ans
 };
 console.log(isDuplicate[1,1,2,3,4,5,6,6, 19, 10, 25, 6]);
+
+//Symbol- This datatype is a new primitive data type introduced in
+// JavaScript ES6. Symbols are immutable (they cannot be modified) and one-of-a-kind.
+
+const value1 = Symbol("World");
+const value2 = Symbol("World");
+console.log(value1 === value2);
+
+const uniqueKey = Symbol("uniqueKey");
+
+const obj2 = {
+    [uniqueKey]: 1
+};
+obj2[uniqueKey] = 2;
+console.log(obj2[uniqueKey]);
