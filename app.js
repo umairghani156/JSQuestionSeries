016b6bbc7440ffa8c4a430fe50f1ea7a20b59d88
umairghani156 <umairghani156@gmail.com>
2024-09-10 00:06:12 +0500

    added

--- app.js
+++ app.js
@@ -1557,7 +1557,13 @@ function sortedArray(arr){
    
 
   for (let i = 0; i < arr.length; i++) {
-      
+        let minIndex = i;
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j
+            }
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
   }
   console.log(arr);
   
