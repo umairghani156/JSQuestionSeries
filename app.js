a7dbc123d6ffa6a45f3c87eb1e52ee4c15715930
umairghani156 <umairghani156@gmail.com>
2024-09-10 07:40:50 +0500

    added

--- app.js
+++ app.js
@@ -1620,10 +1620,17 @@
 
 
 function arrayAreEqual(arr1, arr2){
-  return arr1 === arr2
+ if(arr1.length !== arr2.length)
+ {
+   return false
+ }
+
+ return arr1.every((cur, index)=> cur === arr2[index])
 }
 
 console.log(arrayAreEqual([1, 2],[1, 2]));
+console.log(arrayAreEqual([1, 2],[1, 2, 3]));
+
 console.log(arrayAreEqual([],[]));
 
 
