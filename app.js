06e44fe5c9862d7ab1cd2aa4cf78245a3e8a9bca
umairghani156 <umairghani156@gmail.com>
2024-09-11 20:31:02 +0500

    added

--- app.js
+++ app.js
@@ -1814,6 +1814,8 @@ function findMedian(arr){
 }
 console.log(findMedian([5, 3, 9, 1, 7]));
 console.log(findMedian([2, 4, 6, 8]));
+console.log(findMedian([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
 
 
 
