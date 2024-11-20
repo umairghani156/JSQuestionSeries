aa4d92e3be7b3747e42476d6e5a10b33c4bac670
umairghani156 <umairghani156@gmail.com>
2024-11-20 23:41:13 +0500

    added

--- app.js
+++ app.js
@@ -4289,7 +4289,22 @@ function join(arr1, arr2) {
   return finalData  
 }
 
+function join(arr1, arr2) {
+  let data = arr1.concat(arr2).sort((a, b)=> a.id - b.id);
+  let resultObj = {}
+  for(let d of data){
+   let id = d.id
+   if(resultObj[id]){
+     resultObj[id] = {...resultObj[id], ...d}
+   }else{
+     resultObj[id] = d
+   }
 
+  
+  }
+  let finalData = Object.values(resultObj);
+  return finalData  
+}
 
 
 
