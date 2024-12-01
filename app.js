0b3516d0d8726e660f5e9b732f4244b4d0a92b49
umairghani156 <umairghani156@gmail.com>
2024-12-01 23:43:46 +0500

    added

--- app.js
+++ app.js
@@ -4844,24 +4844,7 @@ const data = [
 //   }
 // })
 
-// let arr = [1, 2, 3, 4, 5, 6,7];
 
-// arr.forEach((val)=>{
-//  console.log(val);
-//   if(val === 3){
-//     return val
-    
-//   }
-// })
-let arr = [1, 2, 3, 4, 5, 6,7];
-
-arr.forEach((val)=>{
- console.log(val);
-  if(val === 3){
-    return val
-    
-  }
-})
 
 
 
