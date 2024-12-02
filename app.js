e643bbce1333a550db832dc233a7b2f4dee5d54a
umairghani156 <umairghani156@gmail.com>
2024-12-02 09:18:01 +0500

    added

--- app.js
+++ app.js
@@ -4891,6 +4891,27 @@ const data = [
     
 // }
 
+var fibGenerator = function*() {
+  let a = 0, b = 1;
+  while(true){
+    yield a
+    [a,b] =  [b, a + b];
+  }
+};
+
+
+ const gen = fibGenerator();
+
+ 
+ console.log(gen.next().value); // 0
+ console.log(gen.next().value); // 1
+ console.log(gen.next().value); // 1
+ console.log(gen.next().value); // 1
+ console.log(gen.next().value); // 1
+ console.log(gen.next().value); // 1
+
+ 
+
 
 
 
