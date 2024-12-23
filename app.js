45049f17ada17176a06d66f2fb66f24bc00fd84b
umairghani156 <umairghani156@gmail.com>
2024-12-23 19:37:21 +0500

    extractNumberInString

--- app.js
+++ app.js
@@ -5371,28 +5371,55 @@ const data = [
 
 
 
-Function.prototype.myBind = function(context, ...args) {
-  // Your code here
-   context.fn = this;
-   return function( ...par){
-     context.fn(...par)
-   }
+// Function.prototype.myBind = function(context, ...args) {
+//   // Your code here
+//    context.fn = this;
+//    return function( ...par){
+//      context.fn(...par)
+//    }
  
   
-}
+// }
 
-let obj = {
-  name: "Umair",  
-  mySelf: function(age){
-    console.log(`Hey this is ${this.name} and my age is ${age}`);
+// let obj = {
+//   name: "Umair",  
+//   mySelf: function(age){
+//     console.log(`Hey this is ${this.name} and my age is ${age}`);
     
+//   }
+// }
+// let obj2 = {
+//   name: "Faiz"
+// }
+// let val = obj.mySelf.myBind(obj2);
+// console.log(val(24));
+
+function helloPrac(str){
+  let arr = []
+  let temp = ''; 
+  let n = str.length;
+  for(let i= 0; i < n; i++){
+    if (!isNaN(str[i]) && str[i] !== ' ') { 
+      temp += str[i]; 
+    } else {
+      if (temp) {
+        arr.push(temp);
+        temp = ''; 
+      }
+    }
   }
+  console.log(temp);
+  if(temp){
+    arr.push(temp)
+  }
+  
+  console.log(arr);
+  
+  
+
 }
-let obj2 = {
-  name: "Faiz"
-}
-let val = obj.mySelf.myBind(obj2);
-console.log(val(24));
+console.log(helloPrac("a12245d34hj58"));
+
 
 
 
